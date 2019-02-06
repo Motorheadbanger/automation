c3175c5d05559424749d30279d6e9a4ccd1f062a
Motorheadbanger <dv.fominykh@gmail.com>
2019-02-06 22:40:10 +0300

    Solved Task4

---
+++ Lesson1/Task4/Task4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task4
+{
+    class Task4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Specify the first line: ");
+            string firstLine = Console.ReadLine();
+            Console.WriteLine("Specify the second line: ");
+            string secondLine = Console.ReadLine();
+            Console.WriteLine("Specify the symbol: ");
+            string symbolInput = Console.ReadLine();
+            char symbol = ParseSymbol(symbolInput);
+
+            string resultString = firstLine + secondLine;
+            Console.WriteLine("Index of " + symbol + " in " + resultString + ": " + resultString.LastIndexOf(symbol));
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static char ParseSymbol(String input)
+        {
+            char result;
+            bool isSymbol = false;
+            char[] parsedInput = input.ToCharArray();
+            result = parsedInput[0];
+            do
+            {
+                if (parsedInput.Length == 1)
+                {
+                    isSymbol = true;
+                }
+                if (!isSymbol)
+                {
+                    Console.Write("Not a symbol. Try again: ");
+                    input = Console.ReadLine();
+                    parsedInput = input.ToCharArray();
+                    result = parsedInput[0];
+                }
+            } while (!isSymbol);
+            return result;
+        }
+    }
+}
