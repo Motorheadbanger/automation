8e40174e8cfb9060b877f681805546f13af30bbe
Motorheadbanger <dv.fominykh@gmail.com>
2019-02-06 22:17:54 +0300

    Solved Task3

---
+++ Lesson1/Task3/Task3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Specify the first line: ");
+            string firstLine = Console.ReadLine().ToUpper();
+            Console.WriteLine("Specify the second line: ");
+            String secondLine = Console.ReadLine().ToUpper();
+            Console.WriteLine("Specify the third line: ");
+            String thirdLine = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("String " + firstLine + " starts from " + secondLine + ": " + firstLine.StartsWith(secondLine));
+            Console.WriteLine("String " + firstLine + " ends with " + thirdLine + ": " + firstLine.EndsWith(thirdLine));
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
