4427d1d4387fd64b8f7d00624ff864adb8944e4a
Motorheadbanger <dv.fominykh@gmail.com>
2019-02-06 22:03:19 +0300

    Added a library to reuse code that checks input correctness

---
+++ Lesson1/IsNumber/IsNumberClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IsNumber
+{
+    public class IsNumberClass
+    {
+        public static double ParseNumber(String input)
+        {
+            double result;
+            bool isNumber = false;
+            do
+            {
+                isNumber = Double.TryParse(input, out result);
+                if (!isNumber)
+                {
+                    Console.Write("Not a number. Try again: ");
+                    input = Console.ReadLine();
+                }
+            } while (!isNumber);
+            return result;
+        }
+    }
+}
