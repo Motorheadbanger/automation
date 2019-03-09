f5675eeb05c8f3cefddf26cee2c8ecfca8a46c2e
Motorheadbanger <dv.fominykh@gmail.com>
2019-03-09 18:09:23 +0300

    Solved Task1

---
+++ Lesson5/Lesson5/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson5
+{
+    class Rectangle
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+
+        public Rectangle(double side1, double side2)
+        {
+            Side1 = side1;
+            Side2 = side2;
+        }
+
+        public double AreaCalculator(double x, double y)
+        {
+            double result = x * y;
+            return result;
+        }
+
+        public double PerimeterCalculator(double x, double y)
+        {
+            double result = 2 * x + 2 * y;
+            return result;
+        }
+    }
+}
--- Lesson5/Lesson5/Task1.cs
+++ Lesson5/Lesson5/Task1.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ParseNumber;
 
 namespace Lesson5
 {
-    class Program
+    class Task1
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Submit the first value: ");
+            double x = ParseNumberClass.ParseNumber(Console.ReadLine());
+            Console.Write("Submit the second value: ");
+            double y = ParseNumberClass.ParseNumber(Console.ReadLine());
+            Console.WriteLine();
+
+            Rectangle rectangle = new Rectangle(x, y);
+            Console.WriteLine($"The rectangle area is {rectangle.AreaCalculator(x, y)}");
+            Console.WriteLine($"The rectangle perimeter is {rectangle.PerimeterCalculator(x, y)}");
+            Console.ReadKey();
         }
     }
 }
